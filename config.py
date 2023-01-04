6904f7b03d6445e70ac552225f06b577384ae5dc
snipermask1234 <121921039+snipermask1234@users.noreply.github.com>
2023-01-04 13:38:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,10 +61,10 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
+USER_REPLY_TEXT = "Only Admins will be replied to direct messages!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(1139907404)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
